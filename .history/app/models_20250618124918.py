76144de262c0bada7c1898089b303f9ded6752df
gitDominusoft <gitversioncontroll@gmail.com>
2025-06-18 12:57:02 +0200

    add models

---
+++ .history/app/models_20250618124918.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Item(models.Model):
+    item_name = models.CharField(max_length=100, null=True, blank=True)
+    item_description = models.TextField(max_length=2000, null=True, blank=True)
+    item_image = models.ImageField(upload_to="item/",null=True, blank=True )
+    
+    def __str__(self):
+        return f"{self.item_name}"
+    
+class Contact(models.Model):
+    contact_name = models.CharField(max_length=100, null=True, blank=True)
+    contact_surname = models.CharField(max_length=100, null=True, blank=True)
+    contact_comment = models.TextField(max_length=2000, null=True, blank=True)
+    
+    
+    def __str__(self):
+        return f"{self.item_name}"
